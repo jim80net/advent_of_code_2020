14196e1871e29ce4f061ea7dbf938e158ab4255c
Jim Park <jimp@scribd.com>
2020-12-03 00:38:46 -0800

    feat: day 2 part 2
    https://adventofcode.com/2020/day/2

--- d2/d2.clj
+++ d2/d2.clj
@@ -14,7 +14,7 @@
   [input]
   (map #(clojure.string/split % #" ") input))
 
-(defn result
+(defn part1_result
   [triple]
   (let [[s_range s_character s_sample] triple
         range (map #(Integer/parseInt %) (clojure.string/split s_range #"-"))
@@ -30,6 +30,31 @@
                      " sample: " sample 
                      " number: " number))
     (if (and (<= number max) (>= number min)) 1 0)))
+
+(defmacro xor
+  ([a b]
+   `(let [a# ~a
+          b# ~b]
+      (if a#
+        (if b# nil a#)
+        (if b# b# nil)))))
+
+(defn result
+  [triple]
+  (let [[s_range s_character s_sample] triple
+        range (map #(Integer/parseInt %) (clojure.string/split s_range #"-"))
+        character (first s_character)
+        sample (vec s_sample)
+        left (nth sample (dec (first range)))
+        right (nth sample (dec (last range)))
+        left? (= left character)
+        right? (= right character)]
+    (.println *err* (str
+                     "left: " left
+                     " right: " right
+                     " character: " character
+                     " sample: " sample))
+    (if (xor left? right?) 1 0)))
 
 (def main
   (let [outcome (map result (parse_input (input :stdin)))]
