c857d3eafbf6bea9161e365c20db3732db5de0cf
Jim Park <jimp@scribd.com>
2020-12-11 11:38:10 -0800

    [d11]  day 11 part 2
    Not terribly proud of the copy pasta boilerplate...
    
    Refactor maybe if I'm bored.

--- d11/src/d11/core.clj
+++ d11/src/d11/core.clj
@@ -52,6 +52,8 @@
      (and
       (= current 1)
       (<= 4 neighbors-count)))))
+
+
   
 (defn swap-if-necessary-chair
   "Evaluates a coordinate and returns the correct chair"
@@ -60,13 +62,15 @@
         should-i? (swap? neighbors-vecvec coord)]
     (if should-i? (chair-swap! current) current)))
 
+
+
 (defn step
   [neighbors-vecvec]
   (vec (map-indexed (fn [row-idx row]
-                         (vec (map-indexed (fn [chair-idx _chair]
-                                                (swap-if-necessary-chair neighbors-vecvec [row-idx chair-idx]))
-                                              row)))
-                       neighbors-vecvec)))
+                      (vec (map-indexed (fn [chair-idx _chair]
+                                          (swap-if-necessary-chair neighbors-vecvec [row-idx chair-idx]))
+                                        row)))
+                    neighbors-vecvec)))
 
 (defn part-1
   [data]
@@ -76,10 +80,78 @@
         [(count (filter (partial = 1) (flatten neighbors-vecvec))) neighbors-vecvec]
         (recur next-vecvec)))))
 
+(defn line-of-sight
+  [neighbors-vecvec delta starting-coord]
+  (let [max-rows (dec (count neighbors-vecvec))
+        max-columns (dec (count (first neighbors-vecvec)))
+        delta-r (first delta)
+        delta-c (last delta)
+        starting-r (first starting-coord)
+        starting-c (last starting-coord)]
+    (loop [my-r (delta-r starting-r)
+           my-c (delta-c starting-c)]
+      (cond
+        (> my-r max-rows) 0
+        (> my-c max-columns) 0
+        (< my-r 0) 0
+        (< my-c 0) 0
+        :else (let [my-seat (get-in neighbors-vecvec [my-r my-c])]
+                (cond
+                  (pos? my-seat) my-seat
+                  (neg? my-seat) my-seat
+                  :else (recur (delta-r my-r) (delta-c my-c))))))))
+
+
+
+(defn part-2-neighbors
+  [neighbors-vecvec coord]
+     (let [[r c] coord]
+       (map #(line-of-sight neighbors-vecvec % coord) [[dec dec]
+                                                       [dec identity]
+                                                       [dec inc]
+                                                       [identity dec]
+                                                       [identity inc]
+                                                       [inc dec]
+                                                       [inc identity]
+                                                       [inc inc]])))
+
+
+(defn part2-swap?
+  "Evaluates a coordinate and returns a boolean"
+  [neighbors-vecvec coord]
+  (let [current (get-in neighbors-vecvec coord)
+        neighbors-count (neighbors->count (part-2-neighbors neighbors-vecvec coord))]
+    (or
+     (and
+      (= current -1)
+      (= 0 neighbors-count))
+     (and
+      (= current 1)
+      (<= 5 neighbors-count)))))
+
+(defn part2-swap-if-necessary-chair
+  "Evaluates a coordinate and returns the correct chair"
+  [neighbors-vecvec coord]
+  (let [current (get-in neighbors-vecvec coord)
+        should-i? (part2-swap? neighbors-vecvec coord)]
+    (if should-i? (chair-swap! current) current)))
+
+(defn part2-step
+  [neighbors-vecvec]
+  (vec (map-indexed (fn [row-idx row]
+                      (vec (map-indexed (fn [chair-idx _chair]
+                                          (part2-swap-if-necessary-chair neighbors-vecvec [row-idx chair-idx]))
+                                        row)))
+                    neighbors-vecvec)))
+
 (defn part-2
   "Given a sorted list of values, return the number of permutations of increments no greater than 3, leading to the (+ maximum-value 3)"
   [data]
-  data)
+  (loop [neighbors-vecvec data]
+    (let [next-vecvec (part2-step neighbors-vecvec)]
+      (if (= neighbors-vecvec next-vecvec)
+        [(count (filter (partial = 1) (flatten neighbors-vecvec))) neighbors-vecvec]
+        (recur next-vecvec)))))
 
 (defn -main
   [& args]
@@ -88,5 +160,5 @@
           p1-outcome (part-1 data)
           p2-outcome (part-2 data)]
       (println (str "part 1: " (first p1-outcome) (comment " from: " (last p1-outcome))))
-      (println (str "part 2: " (comment p2-outcome))))
+      (println (str "part 2: " (first p2-outcome) (comment (last p2-outcome)))))
     nil))
--- d11/test/d11/core_test.clj
+++ d11/test/d11/core_test.clj
@@ -5,8 +5,8 @@
    [clojure.test :refer [deftest is]]
    [d11.core]))
 
-(defn parse-input-test-result
-  ([] (parse-input-test-result "input.test.result0.edn"))
+(defn read-input-test-result
+  ([] (read-input-test-result "input.test.result0.edn"))
   ([filename]
    (try (with-open [r (io/reader filename)]
           (clojure.edn/read (java.io.PushbackReader. r)))
@@ -15,20 +15,27 @@
         (catch RuntimeException e
           (.println *err* (format "Error parsing edn file '%s': %s%n" filename (.getMessage e)))))))
 
-(def test-step0 (parse-input-test-result "input.test.result0.edn"))
-(def test-step1 (parse-input-test-result "input.test.result1.edn"))
-(def test-step2 (parse-input-test-result "input.test.result2.edn"))
-(def test-step3 (parse-input-test-result "input.test.result3.edn"))
-(def test-step4 (parse-input-test-result "input.test.result4.edn"))
-(def test-step5 (parse-input-test-result "input.test.result5.edn"))
+(defn read-input-test
+  ([] (read-input-test "input.test"))
+  ([filename]
+   (try
+     (line-seq (io/reader filename))
+     (catch java.io.IOException e
+       (.println *err* (format "ERROR: Couldn't open '%s': %s%n" "input.test" (.getMessage e)))))))
+
+
+(def test-step0 (read-input-test-result "input.test.result0.edn"))
+(def test-step1 (read-input-test-result "input.test.result1.edn"))
+(def test-step2 (read-input-test-result "input.test.result2.edn"))
+(def test-step3 (read-input-test-result "input.test.result3.edn"))
+(def test-step4 (read-input-test-result "input.test.result4.edn"))
+(def test-step5 (read-input-test-result "input.test.result5.edn"))
+
 
 
 (deftest parse-input-test
-  (try (with-open [r (io/reader "input.test")]
-         (is (= test-step0 (d11.core/parse-input (line-seq r)))))
-       (catch java.io.IOException e
-         (.println *err* (format "ERROR: Couldn't open '%s': %s%n" "input.test" (.getMessage e))))))
-
+         (is (= test-step0 (d11.core/parse-input (read-input-test)))))
+     
 (deftest parse-char-test
   (map (fn [x y] (is (= (d11.core/parse-char x) y))) ["L" "#" "."] [-1 1 0]))
 
@@ -58,3 +65,12 @@
   (is (= test-step5 (d11.core/step test-step4)))
   (is (= test-step5 (d11.core/step test-step5))))
     
+
+(def test-part2-neighbors (d11.core/parse-input (read-input-test "input.test.part2-neighbors")))
+(def test-part2-noneighbors (d11.core/parse-input (read-input-test "input.test.part2-noneighbors")))
+
+(deftest line-of-sight-test (is (= 1 (d11.core/line-of-sight test-part2-neighbors [dec dec] [4 3]))))
+
+(deftest part2-neighbors-test 
+  (is (= '(1 1 1 1 1 1 1 1) (d11.core/part-2-neighbors test-part2-neighbors [4 3])))
+  (is (= '(0 0 0 0 0 0 0 0) (d11.core/part-2-neighbors test-part2-noneighbors [3 3]))))
